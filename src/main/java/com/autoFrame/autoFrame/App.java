9d989f63501bf3300e518e1a5947f283b4cad432
Alex Cruz <alexcruz182@gmail.com>
2020-03-07 20:22:35 -0500

    initial commit

---
+++ src/main/java/com/autoFrame/autoFrame/App.java
@@ -0,0 +1,13 @@
+package com.autoFrame.autoFrame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
