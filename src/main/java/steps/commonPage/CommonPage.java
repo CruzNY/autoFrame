3212c1a90116dae7f5dd5cbdc1b59a8065a95b72
Alex Cruz <alexcruz182@gmail.com>
2020-03-17 22:17:58 -0400

    Created new commonPage for handleing commonPage methods

---
+++ src/main/java/steps/commonPage/CommonPage.java
@@ -0,0 +1,12 @@
+package steps.commonPage;
+
+import org.openqa.selenium.WebElement;
+
+import utils.DriverFactory;
+
+public class CommonPage extends DriverFactory{
+	public WebElement getWebElement(String elementName){
+		WebElement element = null;
+		return element;
+	}
+}
